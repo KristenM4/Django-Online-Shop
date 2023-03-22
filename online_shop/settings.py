890ba43a9fa654bb24012ab99abac0a70e4c1dbc
Kristen Martin <kristen4810@gmail.com>
2023-03-23 07:51:41 +0800

    add media uploads dir, connect Product model to homepageview

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -118,12 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+MEDIA_URL = "/store-media/"
+MEDIA_ROOT = BASE_DIR / "uploads"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.Customer"
 LOGIN_REDIRECT_URL = "home"
--- pages/views.py
+++ pages/views.py
@@ -3,12 +3,19 @@ from django.views.generic.base import TemplateView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .models import Product
 
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_products = Product.objects.all()
+        context["products"] = all_products
+        return context
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
