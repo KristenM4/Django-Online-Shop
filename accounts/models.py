e77693e2d8d55526920a90bdc915db62853fd3d4
Kristen Martin <kristen4810@gmail.com>
2023-03-22 22:34:20 +0800

    create Product, Category, and Order models and make migrations

--- accounts/models.py
+++ accounts/models.py
@@ -9,10 +9,9 @@ class CustomerAddress(models.Model):
     address_two = models.CharField(max_length=300, blank=True)
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=2, blank=True)
-    zipcode = models.IntegerField(max_length=7)
+    zipcode = models.IntegerField()
     country = CountryField()
 
 class Customer(AbstractUser):
     postal_address = models.OneToOneField(CustomerAddress, null=True, on_delete=models.SET_NULL)
     date_of_birth = models.DateField()
-    username = models.CharField(max_length=30, null=True)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import Customer
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField(upload_to="images")
+    description = models.TextField()
+    stock_quantity = models.IntegerField()
+    discount = models.DecimalField(decimal_places=2, max_digits=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    date = models.DateField(auto_now_add=True)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product,)
