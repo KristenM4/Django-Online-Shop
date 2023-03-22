332a1842f06631bed55b39276862f8b958a409c4
Kristen Martin <kristen4810@gmail.com>
2023-03-22 13:42:37 +0800

    add login and signup pages

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
