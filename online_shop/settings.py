734c96cb7f71616b3179fb72a21de4905a0fe27c
Kristen Martin <kristen4810@gmail.com>
2023-03-24 01:37:59 +0800

    install crispy forms and add login page form

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'pages.apps.PagesConfig',
     'accounts.apps.AccountsConfig',
 ]
@@ -133,3 +135,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.Customer"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
