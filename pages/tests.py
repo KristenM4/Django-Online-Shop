59f9f1ea5d9c82e4b5b89bb2668c9ba7d6b670d1
Kristen Martin <kristen4810@gmail.com>
2023-03-23 09:34:38 +0800

    create unit test for Product and Category models

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,26 @@
 from django.test import TestCase
+from .models import Product, Category
 
 # Create your tests here.
+
+class ProductAndCategoryModelsTest(TestCase):
+    def test_saving_and_retrieving_products(self):
+        category_ = Category(name="Some category")
+        category_.save()
+        product = Product(
+            name="New Product",
+            price="10.99",
+            image="some_image.jpg",
+            description="This is a new product",
+            stock_quantity=15,
+            discount=.00,
+            category=category_,
+        )
+        product.save()
+
+        new_product = Product.objects.all()[0]
+        self.assertEqual(new_product.name, "New Product")
+        self.assertEqual(new_product.category, category_)
 
 
 class HomePageTest(TestCase):
