a20bd48284d447ea6ce66c98face8ce99ad9ab9a
Kristen Martin <kristen4810@gmail.com>
2023-03-25 03:21:59 +0800

    add checkout button to cart

--- pages/views.py
+++ pages/views.py
@@ -37,7 +37,7 @@ def cart_add(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.add(product=product)
-    return redirect("home")
+    return redirect("cart_detail")
 
 
 @login_required(login_url="/accounts/login")
