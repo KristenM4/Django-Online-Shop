c98a2791e5bdb368f0a193f043a23fb5426176b6
Kristen Martin <kristen4810@gmail.com>
2023-03-28 13:02:11 +0800

    collect static and update requirements.txt

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("pages.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
