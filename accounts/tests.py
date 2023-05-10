f16b09bd59f77c3e30a3a1baf4d63e33ecf5f396
Kristen Martin <kristen4810@gmail.com>
2023-05-10 15:37:25 +0800

    unit tests for Customer model and sign up page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,60 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .forms import CustomerCreationForm
+from .views import SignUpView
+
+
+class CustomerModelTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="Kristen",
+            email="kristen@example.com",
+            password="password123",
+            first_name="Kristen",
+            last_name="Test",
+            date_of_birth="1990-01-01",
+        )
+        self.assertEqual(user.username, "Kristen")
+        self.assertEqual(user.email, "kristen@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superadmin",
+            email="superadmin@example.com",
+            password="testpass123",
+            first_name="Kristen",
+            last_name="Test",
+            date_of_birth="1990-01-01",
+        )
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@example.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTests(TestCase):
+    def setUp(self):
+        url = reverse("signup")
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "registration/signup.html")
+        self.assertContains(self.response, "Create an account")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomerCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignUpView.as_view().__name__)
