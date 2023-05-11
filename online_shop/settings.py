7c3021092399c93a686d324175c5db8bd7461905
Kristen Martin <kristen4810@gmail.com>
2023-05-12 02:01:10 +0800

    add env var for DEBUG

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -25,7 +25,7 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = [getenv("APP_HOST"), "localhost", "127.0.0.1"]
 
