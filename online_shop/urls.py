2b2a2bb1c18a4ed5892455db546de80072bbdb72
Kristen Martin <kristen4810@gmail.com>
2023-05-12 11:19:05 +0800

    change admin page path

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("special-admin-page/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("api/", include("apis.urls")),
